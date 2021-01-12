e8c507b993a1bd7fbed8464a79ac1f1749dddc14
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-01-12 15:00:35 +0800

    Import data from JSON file to the homepage

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = 3000
 
 // Require packages used in the project
 const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
 
 // Set up template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -15,7 +16,7 @@ app.use(express.static('public'))
 
 // Set up the routes and corresponding response
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { restaurants: restaurantList.results })
 })
 
 // Start and listener on the Express server
