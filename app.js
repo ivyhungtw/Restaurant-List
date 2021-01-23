8c2b3e1a5fa2f90594f1bd606210fdef22ac4be1
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-01-23 13:33:26 +0800

    feat: add a function to delete restaurant

--- app.js
+++ app.js
@@ -129,6 +129,14 @@ app.post('/restaurants/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 // Start and listener on the Express server
 app.listen(port, () => {
   console.log('Listening')
