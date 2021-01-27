59c87415a5b6e7003ef9880c83c2d7079f3792d7
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-01-27 10:08:59 +0800

    feat: add sort function

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ const router = express.Router()
 // Require modules
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
+const sort = require('./modules/sort')
 
 // Direct to modules
 router.use('/', home)
 router.use('/restaurants', restaurants)
+router.use('/sort', sort)
 
 // Export
 module.exports = router
