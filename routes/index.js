601eec6af42d31c4a68d89d2228fc2ed09f843b5
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-03-09 16:47:51 -0800

    feat: add facebook login feature

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,15 @@ const router = express.Router()
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const users = require('./modules/users')
+const auth = require('./modules/auth')
+
+// Require middleware
 const { authenticator } = require('../middleware/auth')
 
 // Direct to modules
 router.use('/users', users)
 router.use('/restaurants', authenticator, restaurants)
+router.use('/auth', auth)
 router.use('/', authenticator, home)
 
 // Export
